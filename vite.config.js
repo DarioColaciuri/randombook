2169b03fc631c5332996cc4479f38c801a889da6
dariocolaciuri <darioguitarras@gmail.com>
2024-07-31 13:28:37 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/randombook/',  // Nombre del repositorio en GitHub
+  base: '/randombook/', 
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      external: ['firebase']  
+    }
   },
   plugins: [react()],
 });
